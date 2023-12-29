ff07bb1234c837523c08aa6eb5c97e8ac8dcdc26
TulipJani <tkjani20@gmail.com>
2023-12-29 17:36:44 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+function init (){
+  
+gsap.registerPlugin(ScrollTrigger);
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length
+      ? locoScroll.scrollTo(value, 0, 0)
+      : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  },
+  pinType: document.querySelector("#main").style.transform
+    ? "transform"
+    : "fixed",
+});
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.refresh();
+
+}
+
+gsap.from(".btm-part", {
+  height: 0,
+  duration: 1,
+  delay: 1,
+});
+
+init()
